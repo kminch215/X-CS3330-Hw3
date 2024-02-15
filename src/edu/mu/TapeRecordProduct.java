c3b127e3492f51d4582a39016d72a6d7280e52cc
kminch215 <kendraminch1036@gmail.com>
2024-02-14 20:08:20 -0600

    Adding TapeRecordProduct constructor.
    This adds the constructor inherited from the super class MediaProducts.

---
+++ src/edu/mu/TapeRecordProduct.java
@@ -0,0 +1,10 @@
+package edu.mu;
+
+public class TapeRecordProduct extends MediaProduct{
+
+	public TapeRecordProduct(String title, double price, int year, Genre genre) {
+		super(title, price, year, genre);
+		// TODO Auto-generated constructor stub
+	}
+
+}
