4a29113eadbdee2ffc4abdccd2ad211996d3469c
kminch215 <kendraminch1036@gmail.com>
2024-02-14 20:09:52 -0600

    Adding the VinylRecordProduct constructor.
    This adds the constructor inherited from the MediaProduct class so that
    VinylRecordProduct objects can be made.

--- src/edu/mu/VinylRecordProduct.java
+++ src/edu/mu/VinylRecordProduct.java
@@ -1,5 +1,10 @@
 package edu.mu;
 
-public class VinylRecordProduct {
+public class VinylRecordProduct extends MediaProduct{
+
+	public VinylRecordProduct(String title, double price, int year, Genre genre) {
+		super(title, price, year, genre);
+		// TODO Auto-generated constructor stub
+	}
 
 }
