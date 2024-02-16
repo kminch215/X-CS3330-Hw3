62fb033f46f6c82aec4ae29834d966c31afb6745
kminch215 <kendraminch1036@gmail.com>
2024-02-16 16:06:48 -0600

    Changing the VinylRecordProduct constructor
    This takes out the string type from the constructor as as it is not
    needed for the implementation of Homework 3

--- src/edu/mu/VinylRecordProduct.java
+++ src/edu/mu/VinylRecordProduct.java
@@ -2,8 +2,8 @@ package edu.mu;
 
 public class VinylRecordProduct extends MediaProduct{
 
-	public VinylRecordProduct(String type, String title, double price, int year, Genre genre) {
-		super(type, title, price, year, genre);
+	public VinylRecordProduct(String title, double price, int year, Genre genre) {
+		super(title, price, year, genre);
 		// TODO Auto-generated constructor stub
 	}
 
