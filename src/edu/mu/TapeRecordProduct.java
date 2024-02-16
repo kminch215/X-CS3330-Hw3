4889f28f74c6ebcb2079fab42324f863cd1bc963
kminch215 <kendraminch1036@gmail.com>
2024-02-16 16:07:34 -0600

    Changing the TapeRecordProduct constructor
    This takes out the string type from the constructor as as it is not
    needed for the implementation of Homework 3

--- src/edu/mu/TapeRecordProduct.java
+++ src/edu/mu/TapeRecordProduct.java
@@ -2,8 +2,8 @@ package edu.mu;
 
 public class TapeRecordProduct extends MediaProduct{
 
-	public TapeRecordProduct(String type, String title, double price, int year, Genre genre) {
-		super(type, title, price, year, genre);
+	public TapeRecordProduct(String title, double price, int year, Genre genre) {
+		super(title, price, year, genre);
 		// TODO Auto-generated constructor stub
 	}
 
