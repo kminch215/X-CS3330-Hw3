6ed9018708bf18b09574bfa389402232b8126e2b
Samantha Whitaker <s.whita6@gmail.com>
2024-02-16 21:10:12 -0600

    Updated CD and Tape constructors

--- src/edu/mu/VinylRecordProduct.java
+++ src/edu/mu/VinylRecordProduct.java
@@ -1,11 +1,8 @@
 package edu.mu;
 
 public class VinylRecordProduct extends MediaProduct {
-    public VinylRecordProduct(String type, String title, double price, int year, Genre genre) {
-        super(type, title, price, year, genre);
+    public VinylRecordProduct(String title, double price, int year, Genre genre) {
+        super(title, price, year, genre);
     }
 
-    public VinylRecordProduct(VinylRecordProduct other) {
-        super(other.type, other.title, other.price, other.year, other.genre);
-    }
 }
