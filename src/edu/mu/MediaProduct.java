c78da741929de21fe1f23e37bb5249aec8ea836d
Samantha Whitaker <s.whita6@gmail.com>
2024-02-16 22:20:36 -0600

    Regenerated getters and setters

--- src/edu/mu/MediaProduct.java
+++ src/edu/mu/MediaProduct.java
@@ -13,30 +13,38 @@ public class MediaProduct {
         this.genre = genre;
     }
 
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle() {
-		System.out.println(title);
-    }
-    public double getPrice() {
-        return price;
-    }
-    public void setPrice() {
-		System.out.println(price);
-    }
-    public int getYear() {
-        return year;
-    }
-    public void setYear() {
-		System.out.println(year);
-    }
-    public Genre getGenre() {
-        return genre;
-    }
-    public void setGenre() {
-		System.out.println(genre);
-    }
+    
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public Genre getGenre() {
+		return genre;
+	}
+
+	public void setGenre(Genre genre) {
+		this.genre = genre;
+	}
 
 	@Override
 	public String toString() {
