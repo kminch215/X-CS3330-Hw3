8ce4573addad1ec3d4fef3e5e59a7f166d6e3eef
sarahkgreenwood <149001674+sarahkgreenwood@users.noreply.github.com>
2024-02-16 18:57:14 -0600

    Update TapeRecordProduct.java

--- src/edu/mu/TapeRecordProduct.java
+++ src/edu/mu/TapeRecordProduct.java
@@ -1,10 +1,10 @@
 package edu.mu;
 
-public class TapeRecordProduct extends MediaProduct{
-
-	public TapeRecordProduct(String title, double price, int year, Genre genre) {
-		super(title, price, year, genre);
-		// TODO Auto-generated constructor stub
+public class TapeRecordProduct extends MediaProduct {
+    public TapeRecordProduct(String type, String title, double price, int year, Genre genre) {
+        super(type, title, price, year, genre);
+    }
+public TapeRecordProduct(VinylRecordProduct other) {
+    super(other.type, other.title, other.price, other.year, other.genre);
 	}
-
 }
