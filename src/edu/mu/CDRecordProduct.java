c51fcd7d1394806579954ca792dd8091e8a45be4
sarahkgreenwood <149001674+sarahkgreenwood@users.noreply.github.com>
2024-02-16 18:57:30 -0600

    Update CDRecordProduct.java

--- src/edu/mu/CDRecordProduct.java
+++ src/edu/mu/CDRecordProduct.java
@@ -1,10 +1,10 @@
 package edu.mu;
 
-public class CDRecordProduct extends MediaProduct{
-
-	public CDRecordProduct(String title, double price, int year, Genre genre) {
-		super(title, price, year, genre);
-		// TODO Auto-generated constructor stub
-	}
-
+public class CDRecordProduct extends MediaProduct {
+    public CDRecordProduct(String type, String title, double price, int year, Genre genre) {
+        super(type, title, price, year, genre);
+    }
+    public CDRecordProduct(VinylRecordProduct other) {
+        super(other.type, other.title, other.price, other.year, other.genre);
+    }
 }
