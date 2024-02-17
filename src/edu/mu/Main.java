7b7867a494a415a633400a5bee5e6da07d5cd17c
kminch215 <kendraminch1036@gmail.com>
2024-02-16 22:41:18 -0600

    Updated Main code
    Has some of our test code to check if methods are implemented correctly

--- src/edu/mu/Main.java
+++ src/edu/mu/Main.java
@@ -1,14 +1,36 @@
 package edu.mu;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+
+import java.util.ArrayList;
 
 public class Main {  
 	public static void main(String[] args) {
 	// Call the method to read from the CSV file
+		
 	    StockManagerSingleton.getInstance();
 	    
 	    boolean error = StockManagerSingleton.getInstance().intializeStock();
+	    
+	    ArrayList<VinylRecordProduct> name = StockManagerSingleton.getInstance().getVinylRecordList(StockManagerSingleton.getInstance().getArray());
+	    
+	    for(VinylRecordProduct products: name) {
+			System.out.println(products.toString());
+		}
+	    
+	    System.out.println();
+	    
+	    ArrayList<CDRecordProduct> cdName = StockManagerSingleton.getInstance().getCDRecordsList(StockManagerSingleton.getInstance().getArray());
+	    
+	    for(CDRecordProduct products: cdName) {
+			System.out.println(products.toString());
+		}
+	    
+	    System.out.println();
+	    
+	    ArrayList<TapeRecordProduct> tapeName = StockManagerSingleton.getInstance().getTapeRecordList(StockManagerSingleton.getInstance().getArray());
+	    
+	    for(TapeRecordProduct products: tapeName) {
+			System.out.println(products.toString());
+		}
 	    
 	}
 
